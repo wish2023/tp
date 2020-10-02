13e5a93b201548223cd7a4f20b65b0f4dde035cf
Marcus <kafcis.mc@gmail.com>
2020-10-02 12:38:18 +0800

    Added loadTasklistData

--- src/main/java/athena/Storage.java
+++ src/main/java/athena/Storage.java
@@ -23,50 +23,48 @@ public class Storage {
      * Index
      * Restore
      * task and tasklist constructor
-     *
+     * <p>
      * methods given to tasklist
      * saveTaskListData(index) if index -1 then save everything
      * loadTaskListData return taskList and highest index(-1 if no existing data)
-     *
+     * <p>
      * methods needed from Timetable
      * date
      * time
      * task
      * index
-     *
+     * <p>
      * methods given to Timetable
      * saveTimetableData()
      * loadTimetableData return timetable
-     *
+     * <p>
      * methods given to UI
      * lastSaved
      * index
-     *
+     * <p>
      * methods given to Parser
      * ???
-     *
-     *
-     *
-     *
-    * */
+     */
 
     private int size;
 
-    public Storage(int i) {
-        this.size = i;
+    public Storage(String filepath) {
+        this.filePath = filepath;
 
     }
 
-    public void saveTaskListData(taskList list) {
+    public taskList loadTaskListData() {
         File csvFile = new File(filePath);
+        taskList output = null;
         if (csvFile.isFile()) {
             String row;
             BufferedReader csvReader = null;
             try {
                 csvReader = new BufferedReader(new FileReader(filePath));
                 while ((row = csvReader.readLine()) != null) {
-                    String[] data = row.split(",");
-                    /* do something with the data */
+                    String[] data = row.split("\t");
+                    output= new taskList(row);
+                    output.add(data);
                 }
                 csvReader.close();
             } catch (FileNotFoundException e) {
@@ -76,6 +74,8 @@ public class Storage {
             }
 
         }
+        return output;
     }
+
 
 }
