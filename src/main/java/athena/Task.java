9c51a6e7493b690a5de15e6a6e928c25a6f6c7bf
wish2023 <vishesh@u.nus.edu>
2020-10-05 11:01:42 +0800

    Add new features for task

--- src/main/java/athena/Task.java
+++ src/main/java/athena/Task.java
@@ -6,24 +6,58 @@ public class Task {
     private String name;
 
     private String startTime;
-    private int duration = 1;
+    private String duration;
     private String deadline;
 
+    private String recurrence;
     private boolean isDone = false;
-    private Importance importance = Importance.MEDIUM;
-    private String notes = null;
+    private String importance;
+    private String notes;
 
     private String getStatus() {
         return (isDone ? YES : NO);
     }
 
-
-    public Task(String name, String startTime, String deadline) {
+    public Task(String name, String startTime, String duration, String deadline, String recurrence, String importance, String notes) {
         this.name = name;
         this.startTime = startTime;
+        this.duration = duration;
         this.deadline = deadline;
+        this.recurrence = recurrence;
+        this.importance = importance;
+        this.notes = notes;
     }
 
+
+    /**
+     * Edits the features of the task.
+     *
+     * @param name New task name
+     * @param startTime New task start time
+     * @param duration New task duration
+     * @param deadline New task deadline
+     * @param recurrence New task recurrence
+     * @param importance New task importance
+     * @param notes New task notes
+     */
+    public void changeAttributesTo(String name, String startTime, String duration, String deadline, String recurrence, String importance, String notes) {
+        this.name = name;
+        this.startTime = startTime;
+        this.duration = duration;
+        this.deadline = deadline;
+        this.recurrence = recurrence;
+        this.importance = importance;
+        this.notes = notes;
+    }
+
+    /**
+     * Return the importance of the task.
+     *
+     * @return Importance of task
+     */
+    public String getImportance() {
+        return importance;
+    }
 
     /**
      * Marks the task as done.
