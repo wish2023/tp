df141f369805fd033ba0e2552dfbab16f74d69a5
Marcus <kafcis.mc@gmail.com>
2020-10-06 09:44:18 +0800

    no message

--- src/main/java/athena/Athena.java
+++ src/main/java/athena/Athena.java
@@ -3,20 +3,6 @@ import java.util.ArrayList;
 
 public class Athena {
 
-    public static void main(String[] args) {
 
-        TaskList list = new TaskList();
-        ArrayList<String[]> listOfTasks = new ArrayList<String[]>();
-        for(int i=0;i<10;i++) {
-            String[] task = {"this"+i, "is"+i, "donut"+i};
-            listOfTasks.add(task);
-        }
-        for( String[] i: listOfTasks)
-        list.addToList(new Task(i[0],i[1],i[2]));
-        Storage store = new Storage("savedata.csv");
-        store.saveTaskListData(list);
-        TaskList altList = store.loadTaskListData();
-        altList.displayList();
 
-    }
 }
