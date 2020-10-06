40b4b9d3395b0a98a65304ababaa43e51851039b
Marcus <kafcis.mc@gmail.com>
2020-10-06 09:43:13 +0800

    deleted athena class

--- src/main/java/athena/Athena.java
+++ src/main/java/athena/Athena.java
@@ -1,15 +1,22 @@
 package athena;
+import java.util.ArrayList;
 
 public class Athena {
 
     public static void main(String[] args) {
 
-        taskList list = new taskList("string loop hello");
+        TaskList list = new TaskList();
+        ArrayList<String[]> listOfTasks = new ArrayList<String[]>();
+        for(int i=0;i<10;i++) {
+            String[] task = {"this"+i, "is"+i, "donut"+i};
+            listOfTasks.add(task);
+        }
+        for( String[] i: listOfTasks)
+        list.addToList(new Task(i[0],i[1],i[2]));
         Storage store = new Storage("savedata.csv");
         store.saveTaskListData(list);
-        System.out.println(list.toString());
-        list = store.loadTaskListData();
-        System.out.println(list.toString());
+        TaskList altList = store.loadTaskListData();
+        altList.displayList();
 
     }
 }
