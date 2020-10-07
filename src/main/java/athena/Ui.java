a2566ce6b911c7d0f0a510938afe29af34d0baa3
alstontham <alstontham@gmail.com>
2020-10-07 22:40:00 +0800

    fix checkstyle errors

--- src/main/java/athena/Ui.java
+++ src/main/java/athena/Ui.java
@@ -34,9 +34,10 @@ public class Ui {
     public void printTaskAdded(String taskName, String taskStartTime, String taskDuration, String taskDeadline,
                                String taskRecurrence, String taskImportance, String taskNotes) {
         System.out.println("You've successfully added " + taskName + " to your list!\n"
-                + "This task will start at " + taskStartTime + ", last for " + taskDuration + ", and end at " + taskDeadline + ".\n"
+                + "It will start at " + taskStartTime + " and finish on " + taskDeadline + ".\n"
+                + "You should spend a total of " + taskDuration + " hours on it.\n"
                 + "It is set to happen " + taskRecurrence + " and has an importance of " + taskImportance + ".\n"
-                + "Additionally, you've also added these additional notes!\n" + taskNotes +".\n"
+                + "Additionally, you've also added these additional notes!\n" + taskNotes + ".\n"
                 + "Looks like another mission to complete! Let's do it!\n");
     }
 
@@ -62,7 +63,7 @@ public class Ui {
 
     /**
      * Prints out a message verifying that the task the user specified has been edited, and shows the user the
-     * new task details
+     * new task details.
      * @param taskIndex The index of the task on the task list
      * @param taskName The name of the task
      * @param taskStartTime When the task is scheduled to start
@@ -72,8 +73,8 @@ public class Ui {
      * @param taskImportance What is the importance of the task
      * @param taskNotes Any additional notes the user has added to the task
      */
-    public void printTaskEdited(int taskIndex, String taskName, String taskStartTime, String taskDuration, String taskDeadline,
-                                String taskRecurrence, String taskImportance, String taskNotes) {
+    public void printTaskEdited(int taskIndex, String taskName, String taskStartTime, String taskDuration,
+                                String taskDeadline, String taskRecurrence, String taskImportance, String taskNotes) {
         System.out.println("You've changed the details of task number " + taskIndex + ": " + taskName + "?\n"
                 + "Here are the new details of your task!\n"
                 + "Start Time: " + taskStartTime + "\n"
@@ -81,8 +82,8 @@ public class Ui {
                 + "Due Date: " + taskDeadline + "\n"
                 + "Recurrence: " + taskRecurrence + "\n"
                 + "Importance: " + taskImportance + "\n"
-                + "Additional Notes: " + taskNotes + "\n"
-                + "The mistakes of the past have been vanquished!\n");
+                + "Additional Notes: " + taskNotes + "\n");
+        System.out.println("The mistakes of the past have been vanquished!\n");
     }
 
     /**
@@ -108,7 +109,8 @@ public class Ui {
                 + "done INDEX\n"
                 + "e.g. done 2\n\n"
                 + "To edit a task (parameters in square brackets are optional fields):\n"
-                + "edit INDEX [n/NAME] [t/TIME] [d/DURATION] [D/DEADLINE] [r/RECURRENCE] [i/IMPORTANCE] [a/ADDITIONAL-NOTES]\n"
+                + "edit INDEX [n/NAME] [t/TIME] [d/DURATION] [D/DEADLINE] [r/RECURRENCE] [i/IMPORTANCE]"
+                + "[a/ADDITIONAL-NOTES]\n"
                 + "e.g. edit 2 n/Assignment1 t/1100 d/16/09 D/2 r/Monday i/high a/Refer to lecture notes\n\n"
                 + "To delete a task:\n"
                 + "delete INDEX\n"
