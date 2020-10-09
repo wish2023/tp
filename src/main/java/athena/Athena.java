d6ff1ddd701922887f0b8fdc8053fe76bc5ec08d
alstontham <alstontham@gmail.com>
2020-10-09 21:15:06 +0800

    add main class code to take user input

--- src/main/java/athena/Athena.java
+++ src/main/java/athena/Athena.java
@@ -3,15 +3,26 @@ package athena;
 import athena.commands.Command;
 import athena.exceptions.AddException;
 
+import java.util.Scanner;
+
 public class Athena {
-    private static Ui ui;
+    private Ui ui;
+    private Parser parser;
+    private Storage storage;
+    private TaskList oldTask;
 
     public Athena() {
         ui = new Ui();
+        parser = new Parser();
+        storage = new Storage("save.csv", ui);
     }
 
     public static void main(String[] args) {
         Athena athena = new Athena();
+
+        athena.runProgram();
+
+        /*Athena athena = new Athena();
         athena.runProgram();
         Storage storage = new Storage("save.csv", ui);
         TaskList oldTask = new TaskList();
@@ -25,7 +36,7 @@ public class Athena {
         } catch (AddException e) {
             e.getErrorMessage();
         }
-        /*Command command = parser.parse("add n/Assignment2 t/1100 d/16-09 D/2 r/Monday i/high a/Refer to lecture notes");
+        Command command = parser.parse("add n/Assignment2 t/1100 d/16-09 D/2 r/Monday i/high a/Refer to lecture notes");
         command.execute(taskList, athena.ui);
         for (int i = 0; i < 10; i++) {
             command = parser.parse("add n/Assignment2 t/1100 d/16-09 D/2 r/Monday i/high a/Refer to lecture notes");
@@ -43,11 +54,19 @@ public class Athena {
     }
 
     public void runProgram() {
-        // TODO: pass task as argument
-//        ui.printTaskAdded();
-//        ui.printTaskDeleted();
-//        ui.printTaskDone();
-//        ui.printTaskEdited();
+        ui.printWelcomeMessage();
+
+        oldTask = storage.loadTaskListData();
+        Scanner input = new Scanner(System.in);
+        String inputString = input.nextLine();
+
+        while(true) {
+            try {
+                parser.parse(inputString);
+            } catch (Exception e) {
+                e.getMessage();
+            }
+        }
     }
 
 }
