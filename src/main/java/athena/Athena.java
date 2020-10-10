1dba3aee77dd657ccc091e26c60e55bc0c783055
alstontham <alstontham@gmail.com>
2020-10-10 14:57:52 +0800

    tidy main class code

--- src/main/java/athena/Athena.java
+++ src/main/java/athena/Athena.java
@@ -9,7 +9,7 @@ public class Athena {
     private Ui ui;
     private Parser parser;
     private Storage storage;
-    private TaskList oldTask;
+    private TaskList taskList;
 
     public Athena() {
         ui = new Ui();
@@ -19,55 +19,23 @@ public class Athena {
 
     public static void main(String[] args) {
         Athena athena = new Athena();
-
         athena.runProgram();
-
-        /*Athena athena = new Athena();
-        athena.runProgram();
-        Storage storage = new Storage("save.csv", ui);
-        TaskList oldTask = new TaskList();
-        oldTask = storage.loadTaskListData();
-        TaskList taskList = new TaskList();
-        taskList = storage.loadTaskListData();
-        Parser parser = new Parser();
-        try {
-            Command command = parser.parse("add n/ t/ d/16-09 D/2 r/Monday i/high a/Refer to lecture notes");
-            command.execute(taskList, athena.ui);
-        } catch (AddException e) {
-            e.getErrorMessage();
-        }
-        Command command = parser.parse("add n/Assignment2 t/1100 d/16-09 D/2 r/Monday i/high a/Refer to lecture notes");
-        command.execute(taskList, athena.ui);
-        for (int i = 0; i < 10; i++) {
-            command = parser.parse("add n/Assignment2 t/1100 d/16-09 D/2 r/Monday i/high a/Refer to lecture notes");
-            command.execute(taskList, athena.ui);
-        }
-        command = parser.parse("list");
-        command.execute(taskList, athena.ui);
-        command = parser.parse("help");
-        command.execute(taskList, athena.ui);
-        storage.saveTaskListData(taskList);
-        command = parser.parse("delete 2");
-        command.execute(taskList, athena.ui);
-        storage.saveTaskListData(taskList);*/
-
     }
 
     public void runProgram() {
-        Scanner input;
         String inputString;
         Command userCommand;
 
         ui.printWelcomeMessage();
 
-        oldTask = storage.loadTaskListData();
+        taskList = storage.loadTaskListData();
+        Scanner input = new Scanner(System.in);
 
         while (true) {
             try {
-                input = new Scanner(System.in);
                 inputString = input.nextLine();
                 userCommand = parser.parse(inputString);
-                userCommand.execute(oldTask, ui);
+                userCommand.execute(taskList, ui);
             } catch (AddException e) {
                 e.getErrorMessage();
             }
