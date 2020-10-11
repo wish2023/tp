f4987a435744e426e5ff7b48c8ae48771545d477
DESKTOP-BUIJDKP\amand <amandachuasineng@gmail.com>
2020-10-12 00:07:20 +0800

    Add Junit test code for delete and done command in parser

--- src/test/java/athena/ParserTest.java
+++ src/test/java/athena/ParserTest.java
@@ -24,7 +24,7 @@ class ParserTest {
 
     @Test
     public void parse_unknownCommandWord_returnsHelp() {
-        final String input = "unknowncommandword arguments arguments";
+        final String input = "unknown arguments";
         parseAndAssertCommandType(input, HelpCommand.class);
     }
 
@@ -45,13 +45,35 @@ class ParserTest {
     }
 
     /*
+     * Tests for single index argument commands ===============================================================
+     */
+
+    @Test
+    public void parse_deleteCommandNumericArg_indexParsedCorrectly() {
+        final int testNumber = 1;
+        final String input = "delete " + testNumber;
+        final DeleteCommand parsedCommand = parseAndAssertCommandType(input, DeleteCommand.class);
+        final DeleteCommand expectedCommand = new DeleteCommand(testNumber);
+        assertEquals(parsedCommand, expectedCommand);
+    }
+
+    @Test
+    public void parse_doneCommandNumericArg_indexParsedCorrectly() {
+        final int testNumber = 1;
+        final String input = "done " + testNumber;
+        final DoneCommand parsedCommand = parseAndAssertCommandType(input, DoneCommand.class);
+        final DoneCommand expectedCommand = new DoneCommand(testNumber);
+        assertEquals(parsedCommand, expectedCommand);
+    }
+
+    /*
      * Utility methods ====================================================================================
      */
 
     /**
-     * Parses input and asserts the class/type of the returned command object.
+     * Parses input and asserts the class of the returned command object.
      *
-     * @param input to be parsed
+     * @param input                to be parsed
      * @param expectedCommandClass expected class of returned command
      * @return the parsed command object
      */
