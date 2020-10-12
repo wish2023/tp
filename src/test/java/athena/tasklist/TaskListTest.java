964bc37a77fc838bddb3fc1dc4d077de8140c87c
wish2023 <vishesh@u.nus.edu>
2020-10-12 23:10:01 +0800

    Create test case for editTask()

--- src/test/java/athena/tasklist/TaskListTest.java
+++ src/test/java/athena/tasklist/TaskListTest.java
@@ -1,10 +1,23 @@
 package athena.tasklist;
 
+import athena.Importance;
+import athena.Parser;
+import athena.TaskList;
+import athena.Ui;
+import athena.task.Task;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TaskListTest {
+
+    private TaskList taskList;
+
+    @BeforeEach
+    public void setUp() {
+        taskList = new TaskList();
+    }
 
     @Test
     // Check for normal deletion, and then check for a deletion with index out of range
@@ -14,11 +27,23 @@ class TaskListTest {
     }
 
     @Test
-    // Check for edit changing all attributes, edit changing some attributes, edit changing no attributes
-    // Check for edit using invalid index
-    // TODO ^^
     void editTask() {
+        int index = 0;
+        Task task = new Task("Assignment1", "1100",
+                "2 hours", "16-09-2020", "13-10-2020", Importance.valueOf("high".toUpperCase()),
+                "Refer to slides", index);
+        taskList.addTask(task);
 
+        Task expectedTask = new Task("Assignment2", "1200",
+                "4 hours", "16-11-2020", "13-10-2020", Importance.valueOf("low".toUpperCase()),
+                "I have changed", index);
+
+        taskList.editTask(index, "Assignment2", "1200",
+                "4 hours", "16-11-2020", "13-10-2020", Importance.valueOf("low".toUpperCase()),
+                "I have changed");
+
+
+        assertEquals(taskList.at(index), expectedTask);
     }
 
     @Test
