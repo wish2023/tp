a51213a78bdd9cdc4d4cb19dff76781f64e280e8
wish2023 <vishesh@u.nus.edu>
2020-10-12 17:43:27 +0800

    Create TaskListTest.java

---
+++ src/test/java/athena/tasklist/TaskListTest.java
@@ -0,0 +1,22 @@
+package athena.tasklist;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskListTest {
+
+    @Test
+    void deleteTask() {
+
+    }
+
+    @Test
+    void editTask() {
+
+    }
+
+    @Test
+    void getFilteredList() {
+    }
+}
