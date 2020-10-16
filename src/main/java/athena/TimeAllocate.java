81a0c27a667d00735068ac71a7afe8828b99c621
Marcus <kafcis.mc@gmail.com>
2020-10-16 12:39:56 +0800

    Updated classes

--- src/main/java/athena/TimeAllocate.java
+++ src/main/java/athena/TimeAllocate.java
@@ -1,2 +1,57 @@
-package athena;public class TimeAllocate {
+package athena;
+
+import athena.task.Task;
+import athena.task.taskfilter.FlexibleTimeFilter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class TimeAllocate {
+    ArrayList<Task> flexibleTask;
+    ArrayList<Task> fixedTask;
+    LocalDateTime currentTime;
+    LocalDateTime nextTime;
+    int duration;
+
+    /**
+     * This is run manually by the user to generate a timetable that they like.
+     * option to auto assign at every input, reassignment at the end is possible
+     * need to maintain the flexibility of the tasks to allow for multiple reruns
+     *
+     * @param tasks tasks in the current TaskList
+     */
+    public TimeAllocate(TaskList tasks) {
+        this.flexibleTask = tasks.getFilteredList(new FlexibleTimeFilter(true)).getTasks();
+        this.fixedTask = tasks.getFilteredList(new FlexibleTimeFilter(false)).getTasks();
+
+    }
+
+// Psuedocode for allocation runs might refactor into a command class
+//    public void runAllocate() {
+//        Collections.sort(fixedTask, earlier());
+//        Collections.sort(flexibleTask,priority());
+//        //priority sorts according to importance(high to low) then duration(high to low)
+//        //might need toposort
+//
+//        for (int i = 0; i < fixedTask.size() - 1; i++) {
+//            currentTime = fixedTask.get(i).getStartTime() + fixedTask.get(i).getDuration();
+//            nextTime = fixedTask.get(i + 1).getStartTime();
+//            duration = nextTime-currentTime;
+//
+//            // select task with best fit into the time
+//            // select important task then long tasks then check for space
+//            // if there is no time left then continue
+//            // if not then choose another task that can go in
+//            // ?? not sure if want to go through the entire list to get 0 time wastage
+//            // failure cases will be unable to meet deadline
+//            // set time for flexible tasks
+//            // repeat until all flexible tasks are assigned
+//
+//
+//        }
+//    }
+
+
 }
--- src/main/java/athena/task/taskfilter/FlexibleTimeFilter.java
+++ src/main/java/athena/task/taskfilter/FlexibleTimeFilter.java
@@ -1,2 +1,31 @@
-package athena.task.taskfilter;public class FlexibleTimeFilter {
+package athena.task.taskfilter;
+
+import athena.task.Task;
+
+public class FlexibleTimeFilter extends TaskFilter {
+
+    private Boolean toggle;
+
+    public FlexibleTimeFilter(Boolean toggle) {
+        this.toggle = toggle;
+    }
+
+    /**
+     * This is to check if the time for this task is flexible.
+     * need to modify this later to access checkFlexible
+     * currently it only checks for an empty getDate of the task
+     *
+     * @param task Task to check.
+     * @return
+     */
+    @Override
+    public boolean isTaskIncluded(Task task) {
+        if (task.getDate() == null) {
+            return toggle;
+        }
+        return !toggle;
+    }
+
+
 }
+
