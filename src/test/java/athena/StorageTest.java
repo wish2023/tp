195297e6e0f4a17ab3ab157a0a4dcbcf470f862f
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2020-10-22 09:12:32 +0800

    TaskList: Add tests for max number assignment

--- src/test/java/athena/StorageTest.java
+++ src/test/java/athena/StorageTest.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
