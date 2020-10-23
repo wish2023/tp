6b20ea3acb368950e8f4e007ea01c1c25fe98b9e
alstontham <alstontham@gmail.com>
2020-10-23 16:51:01 +0800

    refactor color text code

--- src/main/java/athena/ui/Ui.java
+++ src/main/java/athena/ui/Ui.java
@@ -1,5 +1,6 @@
-package athena;
+package athena.ui;
 
+import athena.Importance;
 import athena.task.Task;
 import athena.timetable.Timetable;
 import org.fusesource.jansi.AnsiConsole;
@@ -8,14 +9,16 @@ import org.fusesource.jansi.AnsiConsole;
  * Represents all of the text that the user sees on the command line.
  */
 public class Ui {
+    private Color color;
     private static final String ANSI_RESET = "\u001B[0m";
 
     public Ui() {
         AnsiConsole.systemInstall();
+        color = new Color();
     }
 
     public void printAthenaLogo() {
-        System.out.println(colourTextPurple("     ___   .__________.  __    __   _______ .__   __.      ___      \n"
+        System.out.println(color.toPurple("     ___   .__________.  __    __   _______ .__   __.      ___      \n"
                 + "    /   \\  |          | |  |  |  | |   ____||  \\ |  |     /   \\     \n"
                 + "   /  ^  \\ `---|  |---` |  |__|  | |  |__   |   \\|  |    /  ^  \\    \n"
                 + "  /  /_\\  \\    |  |     |   __   | |   __|  |  . `  |   /  /_\\  \\   \n"
@@ -27,7 +30,7 @@ public class Ui {
      * Prints the first message that the user sees upon launching the application.
      */
     public void printWelcomeMessage() {
-        System.out.println(colourTextPurple("Hello! I'm the Goddess of Wisdom and War, the mighty ATHENA!\n" + "...\n"
+        System.out.println(color.toPurple("Hello! I'm the Goddess of Wisdom and War, the mighty ATHENA!\n" + "...\n"
                 + "Okay okay I'm not a Goddess but I am your Automated Timetable Helper Encourager n' Assistant!\n"
                 + "What can I do? Are you challenging me to a duel?\n" + "...\n"
                 + "Oh you mean in terms of tasks? Just type \"help\" to witness my mighty repertoire!\n"
@@ -47,13 +50,13 @@ public class Ui {
      */
     public void printTaskAdded(String taskName, String taskStartTime, String taskDuration, String taskDeadline,
                                String taskRecurrence, String taskImportance, String taskNotes) {
-        System.out.println("You've successfully added " + colourTextBlue(taskName) + " to your list!\n"
-                + "It will start at " + colourTextBlue(taskStartTime) + " and finish on "
-                + colourTextBlue(taskDeadline) + ".\n"
-                + "You should spend a total of " + colourTextBlue(taskDuration) + " on it.\n"
-                + "It is set to happen " + colourTextBlue(taskRecurrence) + " and has an importance of "
-                + colourTextBlue(taskImportance) + ".\n"
-                + "Additionally, you've also added these notes!\n" + colourTextBlue(taskNotes) + ".\n"
+        System.out.println("You've successfully added " + color.toBlue(taskName) + " to your list!\n"
+                + "It will start at " + color.toBlue(taskStartTime) + " and finish on "
+                + color.toBlue(taskDeadline) + ".\n"
+                + "You should spend a total of " + color.toBlue(taskDuration) + " on it.\n"
+                + "It is set to happen " + color.toBlue(taskRecurrence) + " and has an importance of "
+                + color.toBlue(taskImportance) + ".\n"
+                + "Additionally, you've also added these notes!\n" + color.toBlue(taskNotes) + ".\n"
                 + "Looks like another mission to complete! Let's do it!\n");
     }
 
@@ -64,7 +67,7 @@ public class Ui {
      */
     public void printTaskDone(Task task) {
         System.out.println("A job well done! I've slayed the-- I mean, marked the task "
-                + colourTextBlue(task.getName()) + " as complete!\n");
+                + color.toBlue(task.getName()) + " as complete!\n");
     }
 
     /**
@@ -83,15 +86,15 @@ public class Ui {
     public void printTaskEdited(int taskIndex, String taskName, String taskStartTime, String taskDuration,
                                 String taskDeadline, String taskRecurrence, Importance taskImportance,
                                 String taskNotes) {
-        System.out.println("You've changed the details of task number " + colourTextBlue(Integer.toString(taskIndex))
-                + ": " + colourTextBlue(taskName) + "!\n"
+        System.out.println("You've changed the details of task number " + color.toBlue(Integer.toString(taskIndex))
+                + ": " + color.toBlue(taskName) + "!\n"
                 + "Here are the new details of your task!\n"
-                + "Start Time: " + colourTextBlue(taskStartTime) + "\n"
-                + "Duration: " + colourTextBlue(taskDuration) + "\n"
-                + "Due Date: " + colourTextBlue(taskDeadline) + "\n"
-                + "Recurrence: " + colourTextBlue(taskRecurrence) + "\n"
-                + "Importance: " + colourTextBlue(taskImportance.name()) + "\n"
-                + "Additional Notes: " + colourTextBlue(taskNotes) + "\n"
+                + "Start Time: " + color.toBlue(taskStartTime) + "\n"
+                + "Duration: " + color.toBlue(taskDuration) + "\n"
+                + "Due Date: " + color.toBlue(taskDeadline) + "\n"
+                + "Recurrence: " + color.toBlue(taskRecurrence) + "\n"
+                + "Importance: " + color.toBlue(taskImportance.name()) + "\n"
+                + "Additional Notes: " + color.toBlue(taskNotes) + "\n"
                 + "The mistakes of the past have been vanquished!\n");
     }
 
@@ -101,10 +104,10 @@ public class Ui {
      * @param task The task that the user has deleted.
      */
     public void printTaskDeleted(Task task, String taskRestore) {
-        System.out.println("Are we pretending the task " + colourTextBlue(task.toString())
+        System.out.println("Are we pretending the task " + color.toBlue(task.toString())
                 + " never existed? Very well...\n");
         System.out.println("We both know the human propensity to make mistakes, if you want it back just type:\n"
-                + colourTextBlue(taskRestore) + "\nYou can thank me later!\n");
+                + color.toBlue(taskRestore) + "\nYou can thank me later!\n");
     }
 
     /**
@@ -113,14 +116,14 @@ public class Ui {
      * @param taskDetails The task that the user wants to view.
      */
     public void printTaskDetails(String taskDetails) {
-        System.out.println("Here are the details of your task: " + taskDetails);
+        System.out.println("Here are the details of your task: " + color.toBlue(taskDetails));
     }
 
     /**
      * Prints out a help menu of all available tasks that ATHENA is able to do.
      */
     public void printHelp() {
-        System.out.println(colourTextYellow("Not sure of what I'm capable of doing? "
+        System.out.println(color.toYellow("Not sure of what I'm capable of doing? "
                 + "Well here's a list just for you!\n\n"
                 + "To add a task (parameters in square brackets are optional fields):\n"
                 + "add n/NAME t/TIME [d/DURATION] [D/DEADLINE] [r/RECURRENCE] [i/IMPORTANCE] [a/ADDITIONAL-NOTES]\n"
@@ -147,7 +150,7 @@ public class Ui {
      * Prints a message when there is an error with a task in the storage file.
      */
     public void printInvalidTask() {
-        System.out.println(colourTextRed("One of the tasks in the archives seems to be cursed, "
+        System.out.println(color.toRed("One of the tasks in the archives seems to be cursed, "
                 + "I have disposed of it!\n"));
     }
 
@@ -157,15 +160,15 @@ public class Ui {
      * @param taskNumber Task number given by the user
      */
     public void printTaskNotFound(int taskNumber) {
-        System.out.println(colourTextRed("The task with the label ")
-                + colourTextBlue(Integer.toString(taskNumber)) + colourTextRed(" cannot be found\n"));
+        System.out.println(color.toRed("The task with the label ")
+                + color.toRed(Integer.toString(taskNumber)) + color.toRed(" cannot be found\n"));
     }
 
     /**
      * Prints a message telling user they did not specify either the name or start time of the task.
      */
     public void printAddMissingRequiredParametersException() {
-        System.out.println(colourTextRed("You haven't specified the name or the start time of the task! "
+        System.out.println(color.toRed("You haven't specified the name or the start time of the task! "
                 + "How can we go on a conquest if we don't know what to slay and what time to start?\n"));
     }
 
@@ -173,7 +176,7 @@ public class Ui {
      * Prints a message telling user they did not provide a valid index for the delete command.
      */
     public void printDeleteInvalidIndexException() {
-        System.out.println(colourTextRed("Hmm...not sure what you're trying to delete, "
+        System.out.println(color.toRed("Hmm...not sure what you're trying to delete, "
                 + "but there is no task at that index.\n"));
     }
 
@@ -181,7 +184,7 @@ public class Ui {
      * Prints a message telling user they did not provide an index for the delete command.
      */
     public void printDeleteNoIndexException() {
-        System.out.println(colourTextRed("So you're trying to delete a task...but which one exactly? "
+        System.out.println(color.toRed("So you're trying to delete a task...but which one exactly? "
                 + "You haven't specified.\n"));
     }
 
@@ -189,7 +192,7 @@ public class Ui {
      * Prints a message telling user they did not provide a valid index for the done command.
      */
     public void printDoneInvalidIndexException() {
-        System.out.println(colourTextRed("I'm not sure if you're trying to smoke me or smoke yourself, "
+        System.out.println(color.toRed("I'm not sure if you're trying to smoke me or smoke yourself, "
                 + "but there is no task at that index. "
                 + "You should enter a task index that contains a task.\n"));
     }
@@ -198,7 +201,7 @@ public class Ui {
      * Prints a message telling user they did not provide an index for the done command.
      */
     public void printDoneNoIndexException() {
-        System.out.println(colourTextRed("I know you're really eager, but you need to specify "
+        System.out.println(color.toRed("I know you're really eager, but you need to specify "
                 + "a task to mark as done!\n"));
     }
 
@@ -206,22 +209,24 @@ public class Ui {
      * Prints a message telling user they did not provide a valid index for the view command.
      */
     public void printViewInvalidIndexException() {
-        System.out.println("I'm not sure if you're trying to smoke me or smoke yourself, but there is no task at that "
-                + "index. You should enter a task index that contains a task.\n");
+        System.out.println(color.toRed("I'm not sure if you're trying to smoke me or "
+                + "smoke yourself, but there is no task at that index. You should enter a task index that "
+                + "contains a task.\n"));
     }
 
     /**
      * Prints a message telling user they did not provide an index for the view command.
      */
     public void printViewNoIndexException() {
-        System.out.println("I know you're really eager, but you need to specify a task to view!\n");
+        System.out.println(color.toRed("I know you're really eager, but you need to specify "
+                + "a task to view!\n"));
     }
 
     /**
      * Prints a message telling user they did not provide an index for the edit command.
      */
     public void printEditNoIndexException() {
-        System.out.println(colourTextRed("You need to specify the correct index of the task "
+        System.out.println(color.toRed("You need to specify the correct index of the task "
                 + "you want to edit!\n"));
 
     }
@@ -230,16 +235,16 @@ public class Ui {
      * Prints a message telling user they did not input a command that is recognized by the program.
      */
     public void printInvalidCommandException() {
-        System.out.println(colourTextRed("I know I'm really smart but even I don't know what "
-                + "you're requesting. Maybe if you type ") + colourTextYellow("help")
-                + colourTextRed(", you can see what the valid commands are.\n"));
+        System.out.println(color.toRed("I know I'm really smart but even I don't know what "
+                + "you're requesting. Maybe if you type ") + color.toYellow("help")
+                + color.toRed(", you can see what the valid commands are.\n"));
     }
 
     /**
      * Prints a message telling user their task list is empty.
      */
     public void printEmptyTaskListException() {
-        System.out.println(colourTextRed("You don't have any tasks in your list! You should probably add some in if "
+        System.out.println(color.toRed("You don't have any tasks in your list! You should probably add some in if "
                 + "you want to be productive.\n"));
     }
 
@@ -247,7 +252,7 @@ public class Ui {
      * Prints a message when the user chooses to exit the program.
      */
     public void printExitMessage() {
-        System.out.println(colourTextPurple("Going so soon? Well I'll be here the next time you need me. "
+        System.out.println(color.toPurple("Going so soon? Well I'll be here the next time you need me. "
                 + "Farewell!\n"));
     }
 
@@ -258,29 +263,5 @@ public class Ui {
      */
     public void printTimetable(Timetable timetable) {
         System.out.println(timetable);
-    }
-
-    private String colourTextBlue(String inputString) {
-        String ansiBlue = "\u001B[34m";
-
-        return ansiBlue + inputString + ANSI_RESET;
-    }
-
-    private String colourTextRed(String inputString) {
-        String ansiRed = "\u001B[31m";
-
-        return ansiRed + inputString + ANSI_RESET;
-    }
-
-    private String colourTextPurple(String inputString) {
-        String ansiGreen = "\u001b[35m";
-
-        return ansiGreen + inputString + ANSI_RESET;
-    }
-
-    private String colourTextYellow(String inputString) {
-        String ansiYellow = "\u001b[33m";
-
-        return ansiYellow + inputString + ANSI_RESET;
     }
 }
