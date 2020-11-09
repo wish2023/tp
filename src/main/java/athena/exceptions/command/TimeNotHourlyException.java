cf4fcc0e6cf2e7f12835ffff97a13359b10ee90b
wish2023 <vishesh@u.nus.edu>
2020-11-09 11:54:13 +0800

    Create TimeNotHourlyException

--- src/main/java/athena/exceptions/command/TimeNotHourlyException.java
+++ src/main/java/athena/exceptions/command/TimeNotHourlyException.java
@@ -1,2 +1,21 @@
-package athena.exceptions.command;public class TimeNotHourlyException {
+package athena.exceptions.command;
+
+import athena.ui.AthenaUi;
+
+/**
+ * Exception thrown when user attempts to add in a task that doesn't start at an hour.
+ */
+public class TimeNotHourlyException extends CommandException {
+    public TimeNotHourlyException() {
+
+    }
+
+    /**
+     * Prints an error message telling user that there the time needs to be hourly.
+     */
+    @Override
+    public void printErrorMessage() {
+        AthenaUi athenaUi = new AthenaUi();
+        athenaUi.printTimeNotHourlyException();
+    }
 }
